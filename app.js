beda327ccfcd5c6e39fb245398f46d8d666e5001
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-15 17:03:44 +0900

    6-2!

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var str1 = 'Hello'
-var str2 = 'World!!'
-alert(str1+str2);
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+	alert('みかんの値段がリンゴより安い')
+}
+else if(orange == apple){
+	alert('みかんとリンゴが同じ値段')
+}
+else{
+	alert('みかんの値段がリンゴより高い')
+}
